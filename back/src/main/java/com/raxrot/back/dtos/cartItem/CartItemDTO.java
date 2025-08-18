e7fe833cb4814debe108f73b4353adfd17bb799e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 20:59:25 +0100

    add CartItemDTO

---
+++ back/src/main/java/com/raxrot/back/dtos/cartItem/CartItemDTO.java
@@ -0,0 +1,15 @@
+package com.raxrot.back.dtos.cartItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemDTO {
+    private Long productId;
+    private Integer quantity;
+}
