e7dbefb9dec32ece7a4f4fce2d5b8e59cfb77e50
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 15:09:09 +0100

    add UserRepository

---
+++ back/src/main/java/com/raxrot/back/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package com.raxrot.back.repositories;
+
+import com.raxrot.back.models.AppRole;
+import com.raxrot.back.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.roleName = :role")
+    Page<User> findByRoleName(@Param("role") AppRole role, Pageable pageable);
+}
