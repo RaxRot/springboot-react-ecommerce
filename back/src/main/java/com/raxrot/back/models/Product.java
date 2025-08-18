882af11005ca78fba444e80365fe4555db00b2c1
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 14:42:19 +0100

    add nullable = false to productName

--- back/src/main/java/com/raxrot/back/models/Product.java
+++ back/src/main/java/com/raxrot/back/models/Product.java
@@ -16,6 +16,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
+    @Column(nullable = false)
     private String productName;
     private String image;
     private String description;
