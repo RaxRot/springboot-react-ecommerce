6f1c1e7731e286b551dd79ce7b5e53da13f58dbd
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 17:35:57 +0100

    add AuthUtil

---
+++ back/src/main/java/com/raxrot/back/utils/AuthUtil.java
@@ -0,0 +1,41 @@
+package com.raxrot.back.utils;
+
+import com.raxrot.back.models.User;
+import com.raxrot.back.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    private final UserRepository userRepository;
+    public AuthUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String loggedInEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getUserId();
+    }
+
+    public User loggedInUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+        return user;
+    }
+}
