3dbba218cfc9aa67b7ffc655651159793e2f55f2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 17:21:25 +0100

    fix constructor in UserInfoResponse

--- back/src/main/java/com/raxrot/back/security/dto/UserInfoResponse.java
+++ back/src/main/java/com/raxrot/back/security/dto/UserInfoResponse.java
@@ -23,4 +23,10 @@ public class UserInfoResponse {
         this.username = username;
         this.roles = roles;
     }
+    public UserInfoResponse(Long id, String username,String email, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
 }
