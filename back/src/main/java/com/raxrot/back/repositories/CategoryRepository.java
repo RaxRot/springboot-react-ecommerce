70810ca06703fe7d9712d28a52e56703afc1d96f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 12:47:50 +0100

    add findByCategoryNameIgnoreCase

--- back/src/main/java/com/raxrot/back/repositories/CategoryRepository.java
+++ back/src/main/java/com/raxrot/back/repositories/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.raxrot.back.repositories;
 import com.raxrot.back.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCategoryNameIgnoreCase(String categoryName);
+    Optional<Category> findByCategoryNameIgnoreCase(String categoryName);
 }
