932610c49e2c5c468e7b358f1d825141ec486c0e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-18 17:33:29 +0100

    small fix

--- back/src/main/java/com/raxrot/back/configs/WebSecurityConfig.java
+++ back/src/main/java/com/raxrot/back/configs/WebSecurityConfig.java
@@ -78,7 +78,6 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/public/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
-                                .requestMatchers("/images/**").permitAll()
                                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 .anyRequest().authenticated()
                 );
