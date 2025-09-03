fcfe9fc37836eceb9e17056b6d4aac512b713c45
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 16:42:02 +0100

    add Product

---
+++ back/src/main/java/com/raxrot/back/models/Product.java
@@ -0,0 +1,30 @@
+package com.raxrot.back.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
