10ebc61160cd7663778e268158a04294f865188b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 18:34:07 +0100

    add AdminProductController

---
+++ back/src/main/java/com/raxrot/back/controllers/AdminProductController.java
@@ -0,0 +1,41 @@
+package com.raxrot.back.controllers;
+
+import com.raxrot.back.dtos.ProductFullResponse;
+import com.raxrot.back.dtos.ProductRequest;
+import com.raxrot.back.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/api/admin/products")
+@RestController
+@RequiredArgsConstructor
+public class AdminProductController {
+    private final ProductService productService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProductFullResponse> create(
+            @RequestPart("data") ProductRequest productRequest,
+            @RequestPart("file") MultipartFile file) {
+        ProductFullResponse response = productService.create(file, productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProductFullResponse> update(
+            @PathVariable Long id,
+            @RequestPart("data") ProductRequest productRequest,
+            @RequestPart(value = "file", required = false) MultipartFile file) {
+        ProductFullResponse response = productService.update(id,file, productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.deleteProductById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
