acf558343bc8ebd8420503378ee3c8df274ee0dc
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 20:23:04 +0100

    add CommentResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/CommentResponse.java
@@ -0,0 +1,21 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponse {
+    private Long id;
+    private String text;
+    private int rating;
+    private Long productId;
+    private String username;
+    private LocalDateTime createdAt;
+}
