2be4484b2f9fb618ac8b0a7f2094107ea6a8b9d2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 14:15:05 +0100

    add Address relationship with user

--- back/src/main/java/com/raxrot/back/models/User.java
+++ back/src/main/java/com/raxrot/back/models/User.java
@@ -42,4 +42,7 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Address address;
 }
