822383dfb6984c35b09aa6a6402e789d1fc8d651
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 17:14:08 +0100

    add ProductFullResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/ProductFullResponse.java
@@ -0,0 +1,22 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductFullResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+    private Integer quantity;
+    private String categoryName;
+}
