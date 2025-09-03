a428ef18745c5a4a053ac04bdf2a83e78ff84c5c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 15:49:54 +0100

    add Category

---
+++ back/src/main/java/com/raxrot/back/models/Category.java
@@ -0,0 +1,21 @@
+package com.raxrot.back.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
