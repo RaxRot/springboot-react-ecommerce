f528235eb995e704d117516fb109cd2d5c498a7b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 11:00:31 +0100

    add Page to findByUser_Id

--- back/src/main/java/com/raxrot/back/repositories/CommentRepository.java
+++ back/src/main/java/com/raxrot/back/repositories/CommentRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByProduct_Id(Long productId, Pageable pageable);
-    List<Comment> findByUser_Id(Long userId);
+    Page<Comment> findByUser_Id(Long userId,Pageable pageable);
 }
