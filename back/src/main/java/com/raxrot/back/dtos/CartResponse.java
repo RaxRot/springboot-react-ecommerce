8ab5fee51aa31a20ad3fa8ba46bd0fcbef3c55bb
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 13:04:09 +0100

    add CartResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/CartResponse.java
@@ -0,0 +1,20 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartResponse {
+    private Long id;
+    private String username;
+    private List<CartItemResponse> items;
+    private BigDecimal totalPrice;
+}
