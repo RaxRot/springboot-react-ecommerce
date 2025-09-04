b951254d30cbabe604b7376fb0d3e5a7acab9845
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 16:21:51 +0100

    add OrderResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/OrderResponse.java
@@ -0,0 +1,23 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponse {
+    private Long id;
+    private String status;
+    private BigDecimal totalAmount;
+    private LocalDateTime createdAt;
+    private String username;
+    private List<OrderItemResponse> items;
+}
