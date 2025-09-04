d53dac12a13aeda547ceab3d85d56074c742d28e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 16:22:19 +0100

    add PaymentResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/PaymentResponse.java
@@ -0,0 +1,19 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponse {
+    private String clientSecret;
+    private Long orderId;
+    private BigDecimal totalAmount;
+}
+
