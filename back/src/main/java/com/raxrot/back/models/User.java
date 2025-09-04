b48c384150c10d8a1ccf5f2e35601d5eb7b16fc3
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 13:04:01 +0100

    add Cart Rel

--- back/src/main/java/com/raxrot/back/models/User.java
+++ back/src/main/java/com/raxrot/back/models/User.java
@@ -50,4 +50,7 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
 }
