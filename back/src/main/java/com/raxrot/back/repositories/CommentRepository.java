feff532750bd2ff4907d114d1d28f9931ee14efc
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-04 11:42:06 +0100

    add rating and review count

--- back/src/main/java/com/raxrot/back/repositories/CommentRepository.java
+++ back/src/main/java/com/raxrot/back/repositories/CommentRepository.java
@@ -4,8 +4,15 @@ import com.raxrot.back.models.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByProduct_Id(Long productId, Pageable pageable);
     Page<Comment> findByUser_Id(Long userId,Pageable pageable);
+    @Query("SELECT AVG(c.rating) FROM Comment c WHERE c.product.id = :productId")
+    Double getAverageRatingByProductId(@Param("productId") Long productId);
+
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.product.id = :productId")
+    Long getReviewCountByProductId(@Param("productId") Long productId);
 }
